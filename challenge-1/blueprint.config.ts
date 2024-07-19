5329ab9046d334c9806febb76bdcbbcd93c45b28
Hoi Nam <hoi.nam@iddy.ai>
2024-07-19 12:39:54 +0800

    blueprint config

--- challenge-1/blueprint.config.ts
+++ challenge-1/blueprint.config.ts
@@ -1,7 +1,15 @@
 import { ScaffoldPlugin } from 'blueprint-scaffold';
+import { Config } from '@ton/blueprint';
 
-export const config = {
+export const config: Config = {
   plugins: [
     new ScaffoldPlugin(),
-  ]
+  ],
+//   network: 'testnet'
+//   network: {
+//     endpoint: 'https://testnet.toncenter.com/api/v2/jsonRPC',
+//     type: 'testnet',
+//     // version: 'v2',
+//     // key: 'YOUR_API_KEY',
+// },
 };
